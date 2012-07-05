a0e3e5494ac7082d0e69176f30c1968adeb9fbf1
Leif Johansson <leifj@sunet.se>
2012-07-05 14:39:16 +0200

    support an alternative way to specify remote URLs

--- src/pyff/pipes/remote.py
+++ src/pyff/pipes/remote.py
@@ -2,6 +2,15 @@ __author__ = 'leifj'
 
 def run(md,t,name,args,id):
     for d in args:
-        if type(d) is dict and d.has_key('url'):
+        if type(d) is str or type(d) is unicode:
+            lst = d.split()
+            d = None
+            if len(lst) == 1:
+                d = {'url':lst[0]}
+            elif len(lst) > 1:
+                d = {'url': lst[0],'verify': lst[1]}
+            if d is not None:
+                md.load_url(**d)
+        elif type(d) is dict and d.has_key('url'):
             md.load_url(**d)
     return t
