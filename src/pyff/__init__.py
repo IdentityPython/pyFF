b62ba4c3e05fb01755ddb96677e995c728ddf1f9
Leif Johansson <leifj@sunet.se>
2012-07-06 01:26:26 +0200

    logging

--- src/pyff/__init__.py
+++ src/pyff/__init__.py
@@ -2,23 +2,37 @@ import sys
 import getopt
 from pyff.mdrepo import  MDRepository
 from pyff.pipes import plumbing
+import logging
 
 def main():
     """
     The main entrypoint for pyFF
     """
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+        opts, args = getopt.getopt(sys.argv[1:], 'h', ['help', 'loglevel='])
     except getopt.error, msg:
         print msg
-        print "for help use --help"
+        print 'for help use --help'
         sys.exit(2)
 
     md=MDRepository()
+    loglevel = logging.WARN
+    logfile = None
     for o, a in opts:
-        if o in ("-h", "--help"):
+        if o in ('-h', '--help'):
             print __doc__
             sys.exit(0)
+        elif o in ('--loglevel'):
+            loglevel = getattr(logging, a.upper(), None)
+            if not isinstance(loglevel, int):
+                raise ValueError('Invalid log level: %s' % loglevel)
+        elif o in ('--logfile'):
+            logfile = a
+
+    log_args = {'level': loglevel}
+    if logfile is not None:
+        log_args['filename'] = logfile
+    logging.basicConfig(**log_args)
 
     for p in args:
         plumbing(p).process(md)
