0d6e2670b4d4a0d39ce2d5aa21dc58b764e44339
Leif Johansson <leifj@sunet.se>
2012-07-06 23:30:00 +0200

    support simple form when single arg

--- src/pyff/pipes/select.py
+++ src/pyff/pipes/select.py
@@ -3,4 +3,6 @@ __author__ = 'leifj'
 def run(md,t,name,args,id):
     if args is None:
         args = md.keys()
+    if type(args) is str or type(args) is unicode:
+        args = [args]
     return md.entity_set(args,id)
