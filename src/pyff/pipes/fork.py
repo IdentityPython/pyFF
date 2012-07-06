4f88a6eaaa5e29915d86319f4582241a8aa726f9
Leif Johansson <leifj@sunet.se>
2012-07-07 00:12:36 +0200

    fork implementation and example

--- src/pyff/pipes/fork.py
+++ src/pyff/pipes/fork.py
@@ -1,3 +1,5 @@
+from pyff.pipes import Plumbing
+
 __author__ = 'leifj'
 
 from copy import deepcopy
@@ -6,8 +8,13 @@ def run(md,t,name,args,id):
     """
     Make a copy of the working tree and process the arguments as a pipleline
     """
+    if type(args) is str or type(args) is unicode:
+        args = [args]
     nt = None
     if t is not None:
         nt = deepcopy(t)
+
+    Plumbing(pipeline=args,id=id).process(md)
+
 
 
