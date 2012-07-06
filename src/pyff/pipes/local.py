8a3bbf8d7ec8d6a8db765710787bf89ce4c0eecf
Leif Johansson <leifj@sunet.se>
2012-07-06 23:29:25 +0200

    support simplified for when processing a single source

--- src/pyff/pipes/local.py
+++ src/pyff/pipes/local.py
@@ -3,6 +3,8 @@ __author__ = 'leifj'
 import os
 
 def run(md,t,name,args,id):
+    if type(args) is str or type(d) is unicode:
+        args = [args]
     for d in args:
         if os.path.isdir(d):
             md.load_dir(d)
