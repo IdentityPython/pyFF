b33c3488918d3af60cbd46de0ae7b8c336d2cf17
Leif Johansson <leifj@sunet.se>
2012-09-28 15:09:54 +0200

    cpstats as instrumentation

---
+++ src/pyff/stats.py
@@ -0,0 +1,26 @@
+"""
+
+Cherrypy statistics module
+
+"""
+
+import time
+import logging
+
+__author__ = 'leifj'
+
+# Initialize the repository
+if not hasattr(logging, 'statistics'): logging.statistics = {}
+# Initialize my namespace
+stats = logging.statistics.setdefault('pyFF Statistics', {})
+# Initialize my namespace's scalars and collections
+stats.update({
+    'Enabled': True,
+    'Start Time': time.time(),
+    'MD Requests': 0,
+    'Repository Update Time': None,
+    'Repository Size': 0,
+    'Requests/Second': lambda s: (
+        (s['MD Requests'] / (time.time() - s['Start Time']))),
+})
+
