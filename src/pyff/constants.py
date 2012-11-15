2eb9325127779ab2041370566005dd5b87f33e40
Leif Johansson <leifj@sunet.se>
2012-11-15 21:54:54 +0100

    name format

--- src/pyff/constants.py
+++ src/pyff/constants.py
@@ -3,6 +3,8 @@ Useful constants for pyFF. Mostly XML namespace declarations.
 """
 __author__ = 'leifj'
 
+NF_URI="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"
+
 NS={"md": "urn:oasis:names:tc:SAML:2.0:metadata",
     'ds': 'http://www.w3.org/2000/09/xmldsig#',
     'mdui': "urn:oasis:names:tc:SAML:metadata:ui",
