7e66a7280f43850c47e51267095be9b0ed72958f
Leif Johansson <leifj@sunet.se>
2012-11-22 00:55:08 +0100

    - sprinkle some mdrpi for tests

--- src/pyff/constants.py
+++ src/pyff/constants.py
@@ -9,6 +9,7 @@ NS={"md": "urn:oasis:names:tc:SAML:2.0:metadata",
     'ds': 'http://www.w3.org/2000/09/xmldsig#',
     'mdui': "urn:oasis:names:tc:SAML:metadata:ui",
     'mdattr': "urn:oasis:names:tc:SAML:metadata:attribute",
+    'mdrpi': "urn:oasis:names:tc:SAML:metadata:rpi",
     'saml': "urn:oasis:names:tc:SAML:2.0:assertion"}
 
 ATTRS = {'collection': 'http://pyff-project.org/collection',
