2d2cc1cc4b104a785ac38e0245adf4a61c32f280
Leif Johansson <leifj@sunet.se>
2012-12-02 22:55:54 +0100

    smaller testkey

--- scripts/genkey.sh
+++ scripts/genkey.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-openssl genrsa 4096 > sign.key
-openssl req -x509 -new -subj "/CN=Signer" -key sign.key -out sign.crt
+openssl genrsa 2048 > sign.key
+openssl req -x509 -sha1 -new -subj "/CN=Signer" -key sign.key -out sign.crt
