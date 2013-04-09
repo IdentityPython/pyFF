97dd165fc146302986b2ece20eb835b9d34069f1
Leif Johansson <leifj@sunet.se>
2013-04-09 18:07:45 +0200

    prep 1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.9'
+version = '1.0dev'
 
 install_requires = [
     'lxml >=3.0',
