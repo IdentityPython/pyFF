860ea90216620d402f5f79ed3f3f3d018d0cb3a3
Leif Johansson <leifj@sunet.se>
2013-09-26 15:46:42 +0200

    cleanup

--- src/pyff/site/static/js/pyff.js
+++ src/pyff/site/static/js/pyff.js
@@ -7,8 +7,6 @@
  */
 
 (function( $ ) {
-    if (!(window.console && console.log)) { (function() { var noop = function() {}; var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn']; var length = methods.length; var console = window.console = {}; while (length--) { console[methods[length]] = noop; } }()); }
-
     // send the user directly to the pre-selected idp if that setting exists
 
     function _autoselect() {
