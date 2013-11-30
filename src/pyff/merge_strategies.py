72bc5958905469dd853ef4956f9358a5a59da796
Leif Johansson <leifj@sunet.se>
2013-11-30 02:24:08 +0100

    pep8

--- src/pyff/merge_strategies.py
+++ src/pyff/merge_strategies.py
@@ -4,10 +4,11 @@ Merge strategies
 __author__ = 'leifj'
 
 
-def replace_existing(e1,e2):
+def replace_existing(e1, e2):
     if e1 is not None:
-        e1.getparent().replace(e1,e2)
+        e1.getparent().replace(e1, e2)
 
-def remove(e1,e2):
+
+def remove(e1, e2):
     if e1 is not None:
         e1.getparent().remove(e1)
