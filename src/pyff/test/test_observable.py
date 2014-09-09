0caac99f095732fafeeaf1528276f5b65158a915
Leif Johansson <leifj@sunet.se>
2014-09-09 02:34:28 +0200

    test for Observable class

---
+++ src/pyff/test/test_observable.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from pyff.mdrepo import Observable
+
+
+class TestObservable(TestCase):
+
+    class Foo(Observable):
+        def __init__(self, done=False):
+            super(TestObservable.Foo, self).__init__()
+            self.done = done
+
+
+    def test_create_observable(self):
+        o = TestObservable.Foo()
+        assert(o is not None)
+        assert(isinstance(o, Observable))
+
+    def test_observe(self):
+
+        def _cb(event):
+            assert('a' in event)
+            assert('b' not in event)
+            assert('myself' in event)
+            assert(event['a'] == 1)
+            assert(not event['myself'].done)
+            event['myself'].done = True
+
+        o = TestObservable.Foo()
+        o.subscribe(_cb)
+        o.fire(a=1,myself=o)
+        assert o.done
