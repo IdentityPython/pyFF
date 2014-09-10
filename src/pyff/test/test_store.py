20594ed79e66b1b7c90622da3f3d5e7caf68c188
Leif Johansson <leifj@sunet.se>
2014-09-10 13:01:21 +0200

    a larger volume metadata testcase based on swamid snapshot

--- src/pyff/test/test_store.py
+++ src/pyff/test/test_store.py
@@ -10,8 +10,10 @@ import os
 class TestRedisStore(TestCase):
     def setUp(self):
         self.datadir = resource_filename('metadata', 'test/data')
-        self.xml_source = os.path.join(self.datadir, 'test01.xml')
-        self.t = parse_xml(self.xml_source)
+        self.test01_source = os.path.join(self.datadir, 'test01.xml')
+        self.test01 = parse_xml(self.test01_source)
+        self.swamid_source = os.path.join(self.datadir, 'swamid-2.0-test.xml')
+        self.swamid = parse_xml(self.swamid_source)
 
     def _redis_store(self):
         store = RedisStore()
@@ -27,21 +29,21 @@ class TestRedisStore(TestCase):
         assert (str(store))
         assert (not store.attributes())
 
-    def test_parse(self):
-        assert (self.xml_source is not None)
-        assert (os.path.exists(self.xml_source))
-        assert (self.t is not None)
+    def test_parse_test01(self):
+        assert (self.test01_source is not None)
+        assert (os.path.exists(self.test01_source))
+        assert (self.test01 is not None)
 
-    def test_import_reset(self):
+    def test_import_reset_test01(self):
         store = self._redis_store()
-        store.update(self.t)
+        store.update(self.test01)
         assert (store.size() > 0)
         store.reset()
         assert (store.size() == 0)
 
-    def test_store_attributes(self):
+    def test_store_attributes_test01(self):
         store = self._redis_store()
-        store.update(self.t)
+        store.update(self.test01)
         assert (ATTRS['domain'] in store.attributes())
         assert (ATTRS['role'] in store.attributes())
         assert (ATTRS['collection'] not in store.attributes())
@@ -49,10 +51,10 @@ class TestRedisStore(TestCase):
         assert ('example.net' in store.attribute(ATTRS['domain']))
         assert ('foo.com' not in store.attribute(ATTRS['domain']))
 
-    def test_lookup(self):
+    def test_lookup_test01(self):
         store = self._redis_store()
-        store.update(self.t)
-        entity_id = root(self.t).get('entityID')
+        store.update(self.test01)
+        entity_id = root(self.test01).get('entityID')
         assert (entity_id is not None)
         e = store.lookup(entity_id)
         print "%s: %s" % (entity_id, e)
@@ -61,10 +63,10 @@ class TestRedisStore(TestCase):
         assert (e[0].get('entityID') is not None)
         assert (e[0].get('entityID') == entity_id)
 
-    def test_lookup_intersect(self):
+    def test_lookup_intersect_test01(self):
         store = self._redis_store()
-        store.update(self.t)
-        entity_id = root(self.t).get('entityID')
+        store.update(self.test01)
+        entity_id = root(self.test01).get('entityID')
         assert (entity_id is not None)
         e = store.lookup("{%s}%s+{%s}%s" % (ATTRS['domain'], 'example.com', ATTRS['role'], 'idp'))
         assert (len(e) == 1)
@@ -72,28 +74,36 @@ class TestRedisStore(TestCase):
         assert (e[0].get('entityID') is not None)
         assert (e[0].get('entityID') == entity_id)
 
-    def test_lookup_intersect_empty(self):
+    def test_lookup_intersect_empty_test01(self):
         store = self._redis_store()
-        store.update(self.t)
-        entity_id = root(self.t).get('entityID')
+        store.update(self.test01)
+        entity_id = root(self.test01).get('entityID')
         assert (entity_id is not None)
         e = store.lookup("{%s}%s+{%s}%s" % (ATTRS['domain'], 'example.com', ATTRS['role'], 'sp'))
         assert (len(e) == 0)
 
-    def test_maintain(self):
+    def test_maintain_test01(self):
         store = self._redis_store()
-        store.update(self.t)
-        entity_id = root(self.t).get('entityID')
+        store.update(self.test01)
+        entity_id = root(self.test01).get('entityID')
         assert (entity_id is not None)
         d = dict()
         store.periodic(d)
         assert('Last Periodic Maintenance' in d)
 
+    def test_load_swamid(self):
+        store = self._redis_store()
+        store.update(self.swamid)
+        assert (store.size() == 990)
+        assert (len(store.lookup("{%s}idp" % ATTRS['role'])) == 534)
+
 class TestMemoryStore(TestCase):
     def setUp(self):
         self.datadir = resource_filename('metadata', 'test/data')
-        self.xml_source = os.path.join(self.datadir, 'test01.xml')
-        self.t = parse_xml(self.xml_source)
+        self.test01_source = os.path.join(self.datadir, 'test01.xml')
+        self.test01 = parse_xml(self.test01_source)
+        self.swamid_source = os.path.join(self.datadir, 'swamid-2.0-test.xml')
+        self.swamid = parse_xml(self.swamid_source)
 
     def test_create_store(self):
         store = MemoryStore()
@@ -103,21 +113,21 @@ class TestMemoryStore(TestCase):
         assert (str(store))
         assert (not store.attributes())
 
-    def test_parse(self):
-        assert (self.xml_source is not None)
-        assert (os.path.exists(self.xml_source))
-        assert (self.t is not None)
+    def test_parse_test01(self):
+        assert (self.test01_source is not None)
+        assert (os.path.exists(self.test01_source))
+        assert (self.test01 is not None)
 
-    def test_import_reset(self):
+    def test_import_reset_test01(self):
         store = MemoryStore()
-        store.update(self.t)
+        store.update(self.test01)
         assert (store.size() > 0)
         store.reset()
         assert (store.size() == 0)
 
-    def test_store_attributes(self):
+    def test_store_attributes_test01(self):
         store = MemoryStore()
-        store.update(self.t)
+        store.update(self.test01)
         assert (ATTRS['domain'] in store.attributes())
         assert (ATTRS['role'] in store.attributes())
         assert (ATTRS['collection'] not in store.attributes())
@@ -125,26 +135,26 @@ class TestMemoryStore(TestCase):
         assert ('example.net' in store.attribute(ATTRS['domain']))
         assert ('foo.com' not in store.attribute(ATTRS['domain']))
 
-    def test_entity_dict(self):
-        d = entity_attribute_dict(root(self.t))
+    def test_entity_dict_test01(self):
+        d = entity_attribute_dict(root(self.test01))
         assert ('example.com' in d[ATTRS['domain']])
         assert ('example.net' in d[ATTRS['domain']])
         assert ('foo.com' not in d[ATTRS['domain']])
 
-    def test_lookup(self):
+    def test_lookup_test01(self):
         store = MemoryStore()
-        store.update(self.t)
-        entity_id = root(self.t).get('entityID')
+        store.update(self.test01)
+        entity_id = root(self.test01).get('entityID')
         e = store.lookup(entity_id)
         assert (len(e) == 1)
         assert (e[0] is not None)
         assert (e[0].get('entityID') is not None)
         assert (e[0].get('entityID') == entity_id)
 
-    def test_lookup_intersect(self):
+    def test_lookup_intersect_test01(self):
         store = MemoryStore()
-        store.update(self.t)
-        entity_id = root(self.t).get('entityID')
+        store.update(self.test01)
+        entity_id = root(self.test01).get('entityID')
         e = store.lookup("%s=%s+%s=%s" % (ATTRS['domain'], 'example.com', ATTRS['role'], 'idp'))
         print e
         assert (len(e) == 1)
@@ -152,10 +162,10 @@ class TestMemoryStore(TestCase):
         assert (e[0].get('entityID') is not None)
         assert (e[0].get('entityID') == entity_id)
 
-    def test_lookup_intersect_empty(self):
+    def test_lookup_intersect_empty_test01(self):
         store = MemoryStore()
-        store.update(self.t)
-        entity_id = root(self.t).get('entityID')
+        store.update(self.test01)
+        entity_id = root(self.test01).get('entityID')
         e = store.lookup("%s=%s+%s=%s" % (ATTRS['domain'], 'example.com', ATTRS['role'], 'sp'))
         print e
         assert (len(e) == 0)
