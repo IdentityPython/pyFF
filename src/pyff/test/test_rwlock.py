b471fe34bf827269cf3b8d74d3cd3ee55da6212f
Leif Johansson <leifj@sunet.se>
2014-09-17 12:33:55 +0200

    more lock tests

--- src/pyff/test/test_rwlock.py
+++ src/pyff/test/test_rwlock.py
@@ -11,6 +11,50 @@ class TestReadWriteLock(TestCase):
         self.writer_active = False
         self.exceptions = dict()
 
+    def test_error_on_release_unheld_lock(self):
+        try:
+            self.lock.release()
+            assert False
+        except ValueError:
+            pass
+
+    def timeout_writer(self, timeout=1):
+        try:
+            self.lock.acquireWrite(timeout=timeout)
+            print "thread (writer): %s starting" % current_thread().name
+            self.writer_active = True
+            sleep(1)
+        except Exception, ex:
+            self.exceptions[current_thread().name] = ex
+        finally:
+            try:
+                self.lock.release()
+            except ValueError:  # ignore double release error
+                pass
+
+        self.writer_active = False
+        print "thread: %s exiting" % current_thread().name
+
+    def timeout_reader(self, to_wait_for, timeout=1):
+        try:
+            self.lock.acquireRead(timeout=timeout)
+            assert(not self.writer_active)
+            print "thread (reader): %s starting" % current_thread().name
+            self.readers += 1
+            while to_wait_for - self.readers > 0:
+                assert(not self.writer_active)
+                print "waiting for %d more readers" % (to_wait_for - self.readers)
+                sleep(0.1)
+        except Exception, ex:
+            self.exceptions[current_thread().name] = ex
+        finally:
+            try:
+                self.lock.release()
+            except ValueError:  # ignore double release error
+                pass
+
+        print "thread (reader): %s exiting" % current_thread().name
+
     def writer(self):
         try:
             with self.lock.writelock:
@@ -41,14 +85,16 @@ class TestReadWriteLock(TestCase):
         if t.name in self.exceptions:
             raise self.exceptions[t.name]
 
-    def test_2_readers(self):
+    def test_2_readers_and_3_writers(self):
         w1 = Thread(target=self.writer, name="w1")
         w2 = Thread(target=self.writer, name="w2")
+        w3 = Thread(target=self.timeout_writer, name="w3", args=[0.01])
         r1 = Thread(target=self.reader, name="r1", args=[2])
         r2 = Thread(target=self.reader, name="r2", args=[2])
         w1.start()
         r1.start()
         w2.start()
+        w3.start()
         r2.start()
         w1.join(timeout=60)
         self._raise(w1)
@@ -57,3 +103,11 @@ class TestReadWriteLock(TestCase):
         r2.join(timeout=60)
         self._raise(r2)
         w2.join(timeout=60)
+        w3.join(timeout=60)
+        try:
+            self._raise(w3)
+            assert False
+        except ValueError:
+            pass
+        except RuntimeError:
+            pass
