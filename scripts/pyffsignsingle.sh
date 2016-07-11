e2a9e2a231d92f998dd14696a4ead63526fcb072
r2h2 <rainer@hoerbe.at>
2016-07-11 15:08:00 +0200

    file location via env

--- scripts/pyffsignsingle.sh
+++ scripts/pyffsignsingle.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 # create a signed XML file per EntityDescriptor for ADFS
 
+if [ ! -e $MDSIGN_CERT ]; then
+    echo "MDSIGN_CERT must be set and point to an existing file" && exit 1
+fi
+if [ ! -e $MDSIGN_KEY ]; then
+    echo "MDSIGN_KEY must be set and point to an existing file" && exit 1
+fi
+if [ ! -e $MD_AGGREGATE ]; then
+    echo "MD_AGGREGATE must be set and point to an existing file" && exit 1
+fi
+
 # Step 1. Split aggregate and create an XML and a pipeline file per EntityDescriptor
 /usr/bin/mdsplit.py \
-    -c /etc/pki/pyff/metadata_signing-crt.pem \
-    -k /etc/pki/pyff/metadata_signing-key.pem \
+    -c $MDSIGN_CERT \
+    -k $MDSIGN_KEY \
     -l /var/log/pyffsplit.log \
     -L DEBUG \
-    /var/md_feed/metadata.xml \
+    $MD_AGGREGATE \
     /var/md_feed/split/ \
     /var/md_source/split/
 
