e089e5c0c9b32df403ccf7ebf910be736a44fd51
r2h2 <rainer@hoerbe.at>
2016-07-11 11:53:59 +0200

    added mdsplit functionality

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(name='pyFF',
       entry_points={
           'console_scripts': ['pyff=pyff.md:main', 'pyffd=pyff.mdx:main']
       },
+      scripts=['scripts/mdsplit.py', ],
       message_extractors={'src': [
           ('**.py', 'python', None),
           ('**/templates/**.html', 'mako', None),
