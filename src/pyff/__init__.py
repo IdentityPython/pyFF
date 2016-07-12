1ad46ace0b9a836dd4597ec96ee877e1f9b2be92
r2h2 <rainer@hoerbe.at>
2016-07-12 15:43:58 +0200

    re-activate pkg_resources.require (does not work in dev env)

--- src/pyff/__init__.py
+++ src/pyff/__init__.py
@@ -10,5 +10,4 @@ __copyright__ = "Copyright 2009-2014 SUNET"
 __license__ = "BSD"
 __maintainer__ = "leifj@sunet.se"
 __status__ = "Production"
-# removing following line was a quick fix to thes message "pkg_resources.DistributionNotFound: The 'pyFF' distribution was not found"
-#__version__ = pkg_resources.require("pyFF")[0].version
+__version__ = pkg_resources.require("pyFF")[0].version
