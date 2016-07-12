8bef8a2a54145831d45f64811ce027140e6ef935
r2h2 <rainer@hoerbe.at>
2016-07-12 16:55:26 +0200

    add comment

--- src/pyff/__init__.py
+++ src/pyff/__init__.py
@@ -10,4 +10,7 @@ __copyright__ = "Copyright 2009-2014 SUNET"
 __license__ = "BSD"
 __maintainer__ = "leifj@sunet.se"
 __status__ = "Production"
+# Issue when invoking pyff_mdsplit.pyfrom Pycharm
+# "pkg_resources.DistributionNotFound: The 'pyFF' distribution was not found"
+# remove following line temporarily to test the script
 __version__ = pkg_resources.require("pyFF")[0].version
