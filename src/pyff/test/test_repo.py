bee3abbdeed3aa1ea83b28591483039545128dae
Leif Johansson <leifj@sunet.se>
2017-12-13 17:02:44 +0100

    fix more tests

--- src/pyff/test/test_repo.py
+++ src/pyff/test/test_repo.py
@@ -5,13 +5,15 @@ import os
 
 from pyff.constants import NS
 from pyff.mdrepo import MDRepository
-from pyff.store import MemoryStore
 from pyff.utils import resource_filename, parse_xml, root, hash_id, MetadataException
-
+from pyff.samlmd import set_entity_attributes, is_idp, is_sp, entity_icon, \
+    entity_domains, entity_extended_display, entity_display_name, entity_simple_summary, \
+    metadata_expiration, annotate_entity, sha1_id
 
 class TestRepo(TestCase):
     def setUp(self):
-        self.md = MDRepository(store=MemoryStore)
+        self.md = MDRepository()
+        self.md.store = self.md.store_class()
         self.datadir = resource_filename('metadata', 'test/data')
         self.xml_source = os.path.join(self.datadir, 'test01.xml')
         self.swamid_source = os.path.join(self.datadir, 'swamid-2.0-test.xml')
@@ -25,20 +27,20 @@ class TestRepo(TestCase):
     def test_clone(self):
         entity_id = root(self.t).get('entityID')
         self.md.store.update(root(self.t), entity_id)
-        nmd = self.md.clone()
-        assert (nmd.store.size() == self.md.store.size())
-        assert (nmd.lookup(entity_id) is not None)
+        nstore = deepcopy(self.md.store)
+        assert (nstore.size() == self.md.store.size())
+        assert (nstore.lookup(entity_id) is not None)
 
     def test_sha1_hash(self):
         entity_id = root(self.t).get('entityID')
         self.md.store.update(root(self.t), entity_id)
         e = self.md.lookup(entity_id)
-        assert (self.md.sha1_id(e[0]) == "{sha1}568515f6fae8c8b4d42d543853c96d08f051ef13")
+        assert (sha1_id(e[0]) == "{sha1}568515f6fae8c8b4d42d543853c96d08f051ef13")
         assert (hash_id(e[0], 'sha1', prefix=False) == "568515f6fae8c8b4d42d543853c96d08f051ef13")
 
     def test_entity_attribute(self):
         entity_id = root(self.t).get('entityID')
-        self.md.set_entity_attributes(root(self.t), {"http://ns.example.org": "foo"})
+        set_entity_attributes(root(self.t), {"http://ns.example.org": "foo"})
         self.md.store.update(root(self.t), entity_id)
         e = self.md.lookup("{%s}%s" % ("http://ns.example.org", 'foo'))[0]
         assert (e is not None)
@@ -48,9 +50,9 @@ class TestRepo(TestCase):
         entity_id = root(self.t).get('entityID')
         self.md.store.update(root(self.t), entity_id)
         e = self.md.lookup(entity_id)[0]
-        assert (self.md.is_idp(e))
-        assert (not self.md.is_sp(e))
-        icon = self.md.icon(e)
+        assert (is_idp(e))
+        assert (not is_sp(e))
+        icon = entity_icon(e)
         assert ('url' in icon)
         assert ('https://www.example.com/static/images/umu_logo.jpg' in icon['url'])
         assert ('width' in icon)
@@ -59,43 +61,43 @@ class TestRepo(TestCase):
         assert ('63' == icon['height'])
         assert ('62' != icon['height'])
 
-        domains = self.md.domains(e)
+        domains = entity_domains(e)
         assert ('example.com' in domains)
         assert ('example.net' in domains)
         assert ('idp.example.com' not in domains)
         assert ('foo.com' not in domains)
 
         edup = deepcopy(e)
-        name, desc = self.md.ext_display(e)
+        name, desc = entity_extended_display(e)
         assert(name == 'Example University')
         assert(desc == 'Identity Provider for Example University')
 
-        disp = self.md.display(e)
+        disp = entity_display_name(e)
         assert (disp == 'Example University')
         for elt in e.findall(".//{%s}DisplayName" % NS['mdui']):
             elt.getparent().remove(elt)
 
-        disp = self.md.display(e)
+        disp = entity_display_name(e)
         assert (disp == 'The Example University')
         for elt in e.findall(".//{%s}OrganizationDisplayName" % NS['md']):
             elt.getparent().remove(elt)
 
-        disp = self.md.display(e)
+        disp = entity_display_name(e)
         assert (disp == 'ExampleU')
         for elt in e.findall(".//{%s}OrganizationName" % NS['md']):
             elt.getparent().remove(elt)
 
-        disp = self.md.display(e)
+        disp = entity_display_name(e)
         assert (disp == entity_id)
 
         e = edup
 
-        subs = self.md.sub_domains(e)
+        subs = entity_domains(e)
         assert ('example.com' in subs)
         assert ('example.net' in subs)
         assert ('idp.example.com' not in subs)
 
-        summary = self.md.simple_summary(e)
+        summary = entity_simple_summary(e)
         assert (summary['title'] == 'Example University')
         assert (summary['descr'] == 'Identity Provider for Example University')
         assert (summary['entityID'] == entity_id)
@@ -104,16 +106,16 @@ class TestRepo(TestCase):
         assert ('domains' in summary)
         assert ('id' in summary)
 
-        empty = self.md.simple_summary(None)
+        empty = entity_simple_summary(None)
         assert (not empty)
 
     def test_display(self):
         swamid = root(self.swamid)
         self.md.store.update(swamid, swamid.get('Name'))
         funet_connect = self.md.lookup('https://connect.funet.fi/shibboleth')[0]
-        name, desc = self.md.ext_display(funet_connect)
+        name, desc = entity_extended_display(funet_connect)
         assert(name == 'FUNET E-Meeting Service')
-        dn = self.md.display(funet_connect)
+        dn = entity_extended_display(funet_connect)
 
 
     def test_missing(self):
@@ -124,10 +126,10 @@ class TestRepo(TestCase):
 
     def test_non_metadata(self):
         e = root(self.non_metadata)
-        assert self.md.expiration(e) is None
+        assert metadata_expiration(e) is None
         try:
-            self.md.annotate(e,"kaka","x","y")
-            self.md.set_entity_attributes(e, dict(a=1))
+            annotate_entity(e,"kaka","x","y")
+            set_entity_attributes(e, dict(a=1))
             assert False
         except MetadataException:
             pass
