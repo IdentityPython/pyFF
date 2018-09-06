d4b091bc769b2b811ae3bf134a2e3b5576f2dc3b
Leif Johansson <leifj@sunet.se>
2018-09-06 20:57:35 +0200

    better mirror script

--- scripts/mirror-mdq.sh
+++ scripts/mirror-mdq.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
+usage() {
+   echo
+   echo "Usage:"
+   echo 
+   echo "`basename $0` [-S|-D|-A] [-h] <base MDX URL> <target rsync enpoint>"
+   echo
+   echo "    -S:  only SAML metadata"
+   echo "    -D:  only discojson metadata"
+   echo "    -A:  all metadata"
+   echo "    -h:  show this message"
+   echo
+   echo "Mirror an MDQ service by querying the .well-known/webfinger"
+   echo "endpoint and fetching all links of the specified type. This"
+   echo "tool uses jq to parse JSON and wget to fetch URLs."
+   echo
+   echo "Data is fetched to a local dir and then rsync:ed with --delete"
+   echo "to the target which can be a remote (ssh) location. Optionally"
+   echo "run-parts a set of post-processing scripts in the temporary"
+   echo "directory before rsync."
+   echo
+   exit 1
+}
+
+rel=""
+
+while getopts SAD c; do
+   case $c in
+      S) rel="urn:oasis:names:tc:SAML:2.0:metadata" ;;
+      A) rel="" ;;
+      D) rel="disco-json" ;;
+      h) usage ;;
+   esac
+done
+
 base=$1
 target=$2
+
+if [ "x$base" = "x" -o "x$target" = "x" ]; then
+   usage
+fi
+
 dir=`mktemp -d`
+
+relarg=""
+if [ "x$rel" != "x" ]; then
+   relarg = "?rel=$rel"
+fi
 
 if [ "x${MIRROR_MDQ_POST}" = "x" ]; then
    export MIRROR_MDQ_POST="/etc/mirror-mdq/post.d"
@@ -24,7 +68,7 @@ echo $target | grep -qv '@' && mkdir -p $target
 (
  cd $dir
  WGET_ARGS="--mirror --no-host-directories -q --connect-timeout=10"
- idx_obj=".well-known/webfinger?rel=urn:oasis:names:tc:SAML:2.0:metadata"
+ idx_obj=".well-known/webfinger$relarg"
  if wget $WGET_ARGS "$base/$idx_obj"; then 
     jq -r '.links[].href' < "$idx_obj" | wget $WGET_ARGS -i -
     ret=$?
