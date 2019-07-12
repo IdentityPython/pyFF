08970ca6ea0646e2cc93a8605514cb62492e38ab
Leif Johansson <leifj@sunet.se>
2019-07-12 10:44:57 +0200

    prep 1.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os.path import abspath, dirname, join
 from setuptools import find_packages
 
 __author__ = 'Leif Johansson'
-__version__ = '1.1.0'
+__version__ = '1.1.1dev0'
 
 here = abspath(dirname(__file__))
 README = open(join(here, 'README.rst')).read()
