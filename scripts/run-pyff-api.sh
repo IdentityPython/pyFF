10b59ea90598c8904676499acf1a807f67f4eb9c
Leif Johansson <leifj@sunet.se>
2019-09-25 09:56:44 +0200

    fix defaults in example gunicorn runner

--- scripts/run-pyff-api.sh
+++ scripts/run-pyff-api.sh
@@ -2,4 +2,4 @@
 
 pipeline=$1; shift
 
-gunicorn --preload --log-config logger.ini --bind 0.0.0.0:8000 -t 600 -e PYFF_PIPELINE=$pipeline -e PYFF_STORE_CLASS=pyff.store:RedisWhooshStore -e PYFF_UPDATE_FREQUENCY=300 --threads 4 --worker-tmp-dir=/dev/shm --worker-class=gthread pyff.wsgi:app
+gunicorn --preload --log-config ${PYFF_LOGGER:-examples/debug.ini} --bind 0.0.0.0:8080 -t 600 -e PYFF_PIPELINE=${pipeline:-examples/mdx.fd} -e PYFF_STORE_CLASS=pyff.store:RedisWhooshStore -e PYFF_UPDATE_FREQUENCY=300 --threads 4 --worker-tmp-dir=/dev/shm --worker-class=gthread pyff.wsgi:app
