bbff430067286fce0c6d502a3b145cbb2b4d7403
Leif Johansson <leifj@sunet.se>
2019-11-26 16:59:51 +0100

    cleanup

--- scripts/run-pyff-api.sh
+++ scripts/run-pyff-api.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-pipeline=$1; shift
+pipeline=${1:-examples/mdx.fd}; shift
 
-gunicorn --preload --log-config ${PYFF_LOGGER:-examples/debug.ini} --bind 0.0.0.0:8080 -t 600 -e PYFF_PIPELINE=${pipeline:-examples/mdx.fd} -e PYFF_STORE_CLASS=pyff.store:RedisWhooshStore -e PYFF_UPDATE_FREQUENCY=300 --threads 4 --worker-tmp-dir=/dev/shm --worker-class=gthread pyff.wsgi:app
+
+exec gunicorn --reload --reload-extra-file ${pipeline} --preload --log-config ${PYFF_LOGGER:-examples/debug.ini} --bind 0.0.0.0:8080 -t 600 -e PYFF_PIPELINE=${pipeline} -e PYFF_STORE_CLASS=pyff.store:RedisWhooshStore -e PYFF_UPDATE_FREQUENCY=300 --threads 4 --worker-tmp-dir=/dev/shm --worker-class=gthread pyff.wsgi:app
