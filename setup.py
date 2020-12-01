48fb95c970da65d932d5ab24003cb77e271eb9f7
Leif Johansson <leifj@sunet.se>
2020-12-01 14:21:36 +0100

    cut 1.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os.path import abspath, dirname, join
 from setuptools import find_packages
 
 __author__ = 'Leif Johansson'
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 
 here = abspath(dirname(__file__))
 README = open(join(here, 'README.rst')).read()
