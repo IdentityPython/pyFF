2e94d63c143deaaf8ceff9ae2975925b06286240
Leif Johansson <leifj@sunet.se>
2021-04-01 21:20:56 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os.path import abspath, dirname, join
 from setuptools import find_packages
 
 __author__ = 'Leif Johansson'
-__version__ = '2.0.0pre0'
+__version__ = '2.0.0'
 
 here = abspath(dirname(__file__))
 README = open(join(here, 'README.rst')).read()
