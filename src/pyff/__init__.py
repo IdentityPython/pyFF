309cef224dfe7c676bdca93c13d5b10cf2d8378a
Mikael Frykholm <mifr@sunet.se>
2025-04-07 15:57:10 +0200

    Started removing pkg_resources. Formatting fixes.

--- src/pyff/__init__.py
+++ src/pyff/__init__.py
@@ -2,11 +2,6 @@
 pyFF is a SAML metadata aggregator.
 """
 
-import pkg_resources
+import importlib
 
-__author__ = 'Leif Johansson'
-__copyright__ = "Copyright 2009-2018 SUNET and the IdentityPython Project"
-__license__ = "BSD"
-__maintainer__ = "leifj@sunet.se"
-__status__ = "Production"
-__version__ = pkg_resources.require("pyFF")[0].version
+__version__ = importlib.metadata.version('pyFF')
